94faa2aad2e4419d23ce006e3da32bddda2d3c3a
Gustavo Lubian <gustavolubian@gmail.com>
2022-12-13 10:56:28 -0300

    LayoutNetflix

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = "9fec665cc6ed0972d523d440bf25b1ad"
+let img_url = "https://image.tmdb.org/t/p/w500/"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
